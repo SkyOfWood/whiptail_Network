92386f27cd062224036975dee0dd2b5fccbf1ad2
wuchenglin <flynn1994@qq.com>
2021-08-23 10:29:26 +0800

    更新User_network.sh脚本链接

--- create_user.sh
+++ create_user.sh
@@ -8,16 +8,16 @@ userdel -r -f $USER_NAME
 useradd -m $USER_NAME -s '/usr/bin/sudo /usr/local/bin/User_network'
 
 #配置密码
-echo 'network123'|passwd --stdin $USER_NAME
+echo 'network123' | passwd --stdin $USER_NAME
 
 #禁止ssh
 sed -i '/.*DenyUsers.*/d' /etc/ssh/sshd_config
-echo "DenyUsers $USER_NAME" >> /etc/ssh/sshd_config
+echo "DenyUsers $USER_NAME" >>/etc/ssh/sshd_config
 service sshd reload
 
 #sudo免密码
-echo "$USER_NAME ALL=(root)NOPASSWD: /usr/local/bin/User_network" > /etc/sudoers.d/user-$USER_NAME
+echo "$USER_NAME ALL=(root)NOPASSWD: /usr/local/bin/User_network" >/etc/sudoers.d/user-$USER_NAME
 chmod 400 /etc/sudoers.d/user-$USER_NAME
 
-curl -ks https://raw.githubusercontent.com/SkyOfWood/whiptail_User/master/User_network.sh -o /usr/local/bin/User_network
+curl -ks https://raw.githubusercontent.com/SkyOfWood/whiptail_Network/master/User_network.sh -o /usr/local/bin/User_network
 chmod 755 /usr/local/bin/User_network
